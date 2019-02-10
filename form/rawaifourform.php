bb4ccf2bf59aa807ad9f5c6c886e3f9deec10bf3
Aleksandar Milic <Aleksandarmilic1995@gmail.com>
2019-02-10 08:03:12 +0100

    Add test.txt

---
+++ form/rawaifourform.php
@@ -0,0 +1,18 @@
+
+<?php
+
+if (isset($_POST['name']) && isset($_POST['message']) && isset($_POST['email'])) {
+	$name = $_POST['name'];
+	$message = $_POST['message'];
+	$mailFrom = $_POST['email'];
+	$fromDate = $_POST['fromDate'];
+	$toDate = $_POST['toDate'];
+	
+	$mailTo = "info@villasgetaway.com";
+	$headers = "From: ".$message;
+	$txt = " (RAWAI4) You have received an e-mail from ".$name.".\n\n".$mailFrom." From date".$fromDate." to date ".$toDate;
+
+	mail($mailTo, $txt, $headers);
+}
+
+?>
